c8aa0f3cf5c3afb749f5bd918bd1fbc047ee3cda
Animesh Roy <animeshroy@Live.com>
2019-06-24 00:53:05 +0530

    Automate push via script at

--- push.sh
+++ push.sh
@@ -2,7 +2,8 @@ echo Pushing update
 echo adding stuffs
 git add --all
 sleep 2
-git commit -m "auto push via script"
-echo push done!
+git commit -m "Automate push via script at $date"
+echo msg added
 sleep 1
 git push origin master
+echo "--------------DonE-----------"
